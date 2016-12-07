2472e37f2d9ce25a3311e448edbf398693d5b05c
Hasan Korre <hkorre@gmail.com>
2016-12-07 09:13:15 +0000

    hkorre: got more data parsing done yesterday

--- data_parser.py
+++ data_parser.py
@@ -11,13 +11,16 @@ class DataParser:
   """Prepare the data from image files and csv file"""
 
   def __init__(self):
+    self._img_height = 160
+    self._img_width_original = 320
+
     self._filename = 'driving_log.csv'
     self._steering_angles = []
 
   def _grab_data(self):
     self._file_IDs = []
 
-    with open('driving_log.csv', 'rb') as f:
+    with open('driving_log.csv', 'r') as f:
       try:
         reader = csv.reader(f)
         for row in reader:
@@ -26,12 +29,34 @@ class DataParser:
       finally:
         f.close()
 
+  def _format_center_img(self, file_ID):
+    image = mpimg.imread('IMG/center_' + file_ID)
+    start = int(self._img_height/2)
+    end   = self._img_width_original - start
+    image = image[:, start:end, :]
+    return image
+
+
+
   def _combine_imgs(self):
     print('_combining_imgs')
-    print(len(self._steering_angles))
+
+    num_imgs = len(self._steering_angles)
+
+    self._left_imgs = np.zeros((num_imgs, self._img_height, self._img_height, 3))
+    self._center_imgs = np.zeros_like(self._left_imgs)
+    self._right_imgs = np.zeros_like(self._left_imgs)
+
+    for index in range(num_imgs):
+      print(index)
+      self._center_imgs[index] = self._format_center_img(self._file_IDs[index])
+      
+    '''
+    print(self._right_imgs.shape)
+    image_center = self._format_center_img(self._file_IDs[0])
+    print('This image is:', type(image_center), 'with dimesions:', image_center.shape)
+    '''
   
-    image = mpimg.imread('IMG/center_' + self._file_IDs[0])
-    print('This image is:', type(image), 'with dimesions:', image.shape)
  
 
   '''
