d6254b06dc90ff0dcd4ae09524d948e756842b11
Hasan Korre <hkorre@gmail.com>
2016-12-09 07:24:21 +0000

    hkorre: got model to build

--- model.py
+++ model.py
@@ -2,7 +2,10 @@
 
 from keras.applications.vgg16 import VGG16
 from keras.models import Sequential
-from keras.layers import Merge, Flatten
+from keras.layers import Input, Merge, Dense, Lambda
+from keras.layers import Flatten, Activation, Dropout
+from keras import backend as K
+from keras.engine.topology import Layer
 
 
 INPUT_HEIGHT = 160
@@ -16,23 +19,25 @@ VGG_CHANNELS = 3
 '''
 
 
+
 class BehaviorCloner:
   """Create Keras model to train learn from driving images in simulator and 
      learn to control the car on it's own"""
 
-
-
   def __init__(self):
     #TODO
+    return
 
 
   def _grab_data(self):
     #TODO
+    return
 
   def _preprocess_data(self):
     #TODO
     # normalize
     #
+    return
 
     ## Split to training and validation sets?
 
@@ -43,7 +48,7 @@ class BehaviorCloner:
     '''
 
 
-  def build_model(self, n_hiddent1_=512, n_hidden2_=512, pct_drop_=0.5):
+  def build_model(self, n_hidden1_=512, n_hidden2_=512, pct_drop_=0.5):
     pool_size_ = 7
 
 
@@ -52,26 +57,28 @@ class BehaviorCloner:
     left_branch.add(VGG16(include_top=False, weights='imagenet', input_tensor=left_input,
       input_shape=(INPUT_HEIGHT, INPUT_WIDTH, INPUT_CHANNELS)))
     #left_branch.add(AveragePooling2D(pool_size=(pool_size_, pool_size_))
-    #left_branch.add(Flatten())
+    left_branch.add(Flatten())
 
     center_input = Input(shape=(INPUT_HEIGHT, INPUT_WIDTH, INPUT_CHANNELS))
     center_branch = Sequential()
     center_branch.add(VGG16(include_top=False, weights='imagenet', input_tensor=center_input,
       input_shape=(INPUT_HEIGHT, INPUT_WIDTH, INPUT_CHANNELS)))
     #center_branch.add(AveragePooling2D(pool_size=(pool_size_, pool_size_))
-    #center_branch.add(Flatten())
+    center_branch.add(Flatten())
 
     right_input = Input(shape=(INPUT_HEIGHT, INPUT_WIDTH, INPUT_CHANNELS))
     right_branch = Sequential()
     right_branch.add(VGG16(include_top=False, weights='imagenet', input_tensor=right_input,
       input_shape=(INPUT_HEIGHT, INPUT_WIDTH, INPUT_CHANNELS)))
     #right_branch.add(AveragePooling2D(pool_size=(pool_size_, pool_size_))
-    #right_branch.add(Flatten())
+    right_branch.add(Flatten())
 
     merged = Merge([left_branch, center_branch, right_branch], mode='concat')
+    #merged = K.stop_gradient(merged)
 
     self._model = Sequential()
     self._model.add(merged)
+    self._model.add(Lambda(lambda x: K.stop_gradient(x)))
     self._model.add(Dense(n_hidden1_, name='fully_connect1'))
     self._model.add(Activation('relu'))
     self._model.add(Dropout(pct_drop_))
@@ -81,11 +88,6 @@ class BehaviorCloner:
     self._model.add(Dense(1, activation='sigmoid', name='final'))
 
     self._model.summary()
-
-    '''
-    base_model = VGG19(weights='imagenet')
-    model = Model(input=base_model.input, output=base_model.get_layer('block4_pool').output)
-    '''
 
 
 
@@ -100,4 +102,9 @@ class BehaviorCloner:
 
 
 
+if __name__ == '__main__':
+  print('Running main in model.py')
+
+  behavior_cloner = BehaviorCloner()
+  behavior_cloner.build_model()
 
